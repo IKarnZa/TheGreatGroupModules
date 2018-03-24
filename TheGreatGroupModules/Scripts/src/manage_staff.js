69a8337acb927741de73442c0d117a26045f1355
Nisakarn <karn_nisa@hotmail.com>
2018-03-24 20:42:37 +0700

    Update Employee Management

--- TheGreatGroupModules/Scripts/src/manage_staff.js
+++ TheGreatGroupModules/Scripts/src/manage_staff.js
@@ -1,1 +1,109 @@
-﻿
+﻿function Load_DataGrid(data) {
+
+
+    $("#gridListStaffs").dxDataGrid({
+        dataSource: data.data,
+        showColumnLines: true,
+        showRowLines: true,
+        //  rowAlternationEnabled: true,
+        showBorders: true,
+        selection: {
+            mode: "single"
+        },
+        searchPanel: {
+            visible: true,
+            width: 300,
+            placeholder: "ค้นหา..."
+        },
+        filterRow: {
+            visible: false,
+            applyFilter: "auto"
+        },
+        export: {
+            enabled: true,
+            fileName: "File",
+        },
+
+        allowColumnReordering: true,
+        allowColumnResizing: true,
+        columnAutoWidth: true,
+        height: 500,
+        columnFixing: {
+            enabled: true
+        },
+        columns: [
+            {
+                dataField: "StaffID",
+                caption: "ลำดับ",
+                width: 50,
+                alignment: 'center',
+                allowFiltering: false,
+                fixed: false,
+                fixedPosition: 'left',
+            },
+            {
+                dataField: "StaffCode",
+                caption: "รหัสพนักงาน",
+                alignment: 'center',
+                width: 100,
+                fixed: false,
+                fixedPosition: 'left',
+            },
+            {
+                dataField: "StaffFirstName",
+                caption: "ชื่อ",
+                alignment: 'left',
+
+
+            },
+            {
+                dataField: "StaffLastName",
+                caption: "นามสกุล",
+                alignment: 'left',
+
+
+            },
+            {
+                dataField: "StaffRoleName",
+                caption: "ตำแหน่ง",
+                alignment: 'left',
+
+
+            },
+            {
+                dataField: "StaffID",
+                caption: "แก้ไข",
+                alignment: 'center',
+                width: 60,
+                fixed: true,
+                fixedPosition: 'right',
+                cellTemplate: function (container, options) {
+
+                    $("<div>")
+                        .append("<a href='\ListContract?CustomerID=" + options.key.StaffID + "'  title='แก้ไขพนักงาน'  class='btn btn-info btn-circle btn-sm' ><i class='fa fa-pencil'></i></a>")
+                        .appendTo(container);
+                }
+            },
+            {
+                dataField: "ID",
+                caption: "ลบ",
+                alignment: 'center',
+                width: 60,
+                fixed: true,
+                fixedPosition: 'right',
+                cellTemplate: function (container, options) {
+
+                    $("<div>")
+                        .append("<a href='\ListContract?CustomerID=" + options.key.StaffID + "'  title='ลบพนักงาน'  class='btn btn-info btn-circle btn-sm' ><i class='fa fa-trash'></i></a>")
+                        .appendTo(container);
+                }
+            },
+
+        ],
+
+    });
+
+}
+function NewZone() {
+    alert("Success");
+}
--- TheGreatGroupModules/Scripts/src/manage_zone.js
+++ TheGreatGroupModules/Scripts/src/manage_zone.js
@@ -1,1 +1,110 @@
-﻿
+﻿function Load_DataGrid(data) {
+
+
+    $("#gridListZone").dxDataGrid({
+        dataSource: data.data,
+        showColumnLines: true,
+        showRowLines: true,
+        //  rowAlternationEnabled: true,
+        showBorders: true,
+        selection: {
+            mode: "single"
+        },
+        searchPanel: {
+            visible: true,
+            width: 300,
+            placeholder: "ค้นหา..."
+        },
+        filterRow: {
+            visible: false,
+            applyFilter: "auto"
+        },
+        export: {
+            enabled: true,
+            fileName: "File",
+        },
+
+        allowColumnReordering: true,
+        allowColumnResizing: true,
+        columnAutoWidth: true,
+        height: 500,
+        columnFixing: {
+            enabled: true
+        },
+        columns: [
+            {
+                dataField: "ID",
+                caption: "No.",
+                width: 50,
+                alignment: 'center',
+                allowFiltering: false,
+                fixed: false,
+                fixedPosition: 'left',
+            },
+            {
+                dataField: "Code",
+                caption: "รหัสพื้นที่",
+                alignment: 'center',
+                width: 100,
+                fixed: false,
+                fixedPosition: 'left',
+            },
+            {
+                dataField: "Value",
+                caption: "ชื่อพื้นที่",
+                alignment: 'left',
+                
+
+            },
+            {
+                dataField: "ID",
+                caption: "กำหนดพนักงาน",
+                alignment: 'center',
+                width: 100,
+                fixed: true,
+                fixedPosition: 'right',
+                verticalAlignment: 'middle',
+                cellTemplate: function (container, options) {
+
+                    $("<div>")
+                        .append("<a href='\ListContract?CustomerID=" + options.key.ID + "'  title='กำหนดพนักงาน'  class='btn btn-info btn-circle btn-sm' ><i class='fa fa-user' aria-hidden='true'></i></a>")
+                        .appendTo(container);
+                }
+            },
+            {
+                dataField: "ID",
+                caption: "แก้ไข",
+                alignment: 'center',
+                width: 60,
+                fixed: true,
+                fixedPosition: 'right',
+                cellTemplate: function (container, options) {
+
+                    $("<div>")
+                        .append("<a href='\ListContract?CustomerID=" + options.key.ID + "'  title='แก้ไขพื้นที่'  class='btn btn-info btn-circle btn-sm' ><i class='fa fa-pencil'></i></a>")
+                        .appendTo(container);
+                }
+            },
+            {
+                dataField: "ID",
+                caption: "ลบ",
+                alignment: 'center',
+                width: 60,
+                fixed: true,
+                fixedPosition: 'right',
+                cellTemplate: function (container, options) {
+
+                    $("<div>")
+                        .append("<a href='\ListContract?CustomerID=" + options.key.ID + "'  title='ลบพื้นที่'  class='btn btn-info btn-circle btn-sm' ><i class='fa fa-trash'></i></a>")
+                        .appendTo(container);
+                }
+            },
+
+        ],
+
+    });
+
+}
+function NewZone() {
+    alert("Success");
+}
